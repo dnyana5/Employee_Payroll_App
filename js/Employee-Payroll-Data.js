7a06c168b86dc625de57a2dc661684c89eddd213
Dnyaneshwari <dnyaneshwarigaikwad5@gmail.com>
2021-03-03 17:55:04 +0530

    Added:Saving Employee Payroll to Local Storage

--- js/Employee-Payroll-Data.js
+++ js/Employee-Payroll-Data.js
@@ -1,75 +1,76 @@
 class EmployeePayrollData {
 
-    //getter and setter method
-    get id() { 
-      return this._id; 
-    }
-    set id(id) { 
-      this._id = id;
-    }
+  //getter and setter method
+  get id() { 
+   return this._id; 
+}
+set id(id) { 
+   this._id = id;
+}
 
-    get name() {
-      return this._name;
-    }
-    set name(name) {
-      let nameRegex=RegExp('^[A-Z]{1}[a-zA-Z]{2,}$');
-      if(nameRegex.test(name))
-        this._name=name;
-      else throw ' Name is Incorrect ';
-    }
+get name() {
+   return this._name;
+}
+set name(name) {
+   let nameRegex = RegExp('^[A-Z]{1}[a-zA-Z]{2,}$')
+   if(nameRegex.test(name))
+       this._name = name;
+   else throw ' Name is Incorrect ';
+}
 
-    get profilePic() {
-      return this._picture;
-    }
-    set profilePic(profilePic) {
-      this._profilePic = profilePic;
-    }
+get profilePic() {
+   return this._profilePic;
+}
+set profilePic(profilePic) {
+   this._profilePic = profilePic;
+}
 
-    get gender() {
-      return this._gender;
-    }
-    set gender(gender) {
-      this._gender = gender;
-    }
+get gender() {
+   return this._gender;
+}
+set gender(gender) {
+   this._gender = gender;
+}
 
-    get department() {
-      return this._department;
-    }
-    set department(department) {
-      this._department = department;
-    }
+get department() {
+   return this._department;
+}
+set department(department) {
+   this._department = department;
+}
 
-    get salary() {
-      return this._salary;
-    }
-    set salary(salary) {
-      this._salary = salary;
-    }
+get salary() {
+   return this._salary;
+}
+set salary(salary) {
+   this._salary = salary;
+}
 
-    get notes() {
-      return this._notes;
-    }
-    set notes(note) {
-      this._notes = note;
-    }
+get notes() {
+   return this._notes;
+}
+set notes(note) {
+   this._notes = note;
+}
 
-    get startDate() {
-      return this._startDate;
-    }
-    set startDate(startDate) {
-      if(startDate.getMonth() <= (new Date()).getMonth()  && startDate.getDay() <= (new Date()).getDay() && startDate.getFullYear() <= (new Date()).getFullYear())
-          this._startDate = startDate;
-      else{ 
-            throw "Invalid Start date ";
-        }
-    }
+get startDate() {
+   return this._startDate;
+}
+set startDate(startDate) {
+   let now = new Date();
+   console.log(now);
+   console.log(startDate);
+   if(startDate > now ) throw 'Start Date is a Future Date'
+   else
+   this._startDate = startDate;
+}
 
-    //method
-    toString() {
-      const options = { year: 'numeric', month: 'long', day: 'numeric' };
-      const empDate = !this.startDate ? "undefined" : this.startDate.toLocalDateString("en-US", options);
-      return "id = "+this.id +", name=" + this.name + ", gender=" + this.gender + 
-             ", profilePic=" + this.profilePic + ", department=" + this.department +
-              ", salary=" + this.salary + ", startDate=" + empDate + ", notes=" + this._notes;
-    }
+//method
+toString() {
+   const options = { year: 'numeric', month: 'long', day: 'numeric' };
+   const empDate = !this.startDate ? "undefined" : this.startDate.toLocalDateString("en-US", options);
+   return "id = "+this.id +", name=" + this.name + ", gender=" + this.gender + 
+           ", profilePic=" + this.profilePic + ", department=" + this.department +
+           ", salary=" + this.salary + ", startDate=" + empDate + ", notes=" + this._notes;
+ }
 }
