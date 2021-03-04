3f21320fa9ef8ce4ef260cb50e41d8bc9c96af07
Dnyaneshwari <dnyaneshwarigaikwad5@gmail.com>
2021-03-05 03:47:36 +0530

    Added:Display Employee Details from JSON object

--- js/home.js
+++ js/home.js
@@ -1,34 +1,69 @@
 window.addEventListener('DOMContentLoaded', (event)=>{
-    createInnerHtml();
+  createInnerHtml();
 });
 
 const createInnerHtml = () => {
-  const innerHtml = `
-    <tr>
-      <th></th>
-      <th>Name</th>
-      <th>Gender</th>
-      <th>Department</th>
-      <th>Salary</th>
-      <th>Start Date</th>
-      <th>Actions</th>
-    </tr>
-    <tr>
-      <td><img class="profile" alt="" src="../assets/profile-images/Ellipse -1.png">
+  const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th>" +
+  "<th>Salary</th><th>Start Date</th><th>Actions</th>";
+  let innerHtml = `${headerHtml}`;
+  let empPayrollList = creteEmployeePayrollJSON();
+  for (const empPayrollData of empPayrollList) {
+      innerHtml = `${innerHtml}
+   <tr>
+      <td><img class="profile" alt="" src="${empPayrollData._profilePic}">
       </td>
-      <td>Dnyaneshwari</td>
-      <td>Female</td>
-      <td><div class="dept-label">Engineer</div>
-      <div class="dept-label">Finance</div></td>
-      <td>4000000</td>
-      <td>1 Nov 2020</td>
+      <td>${empPayrollData._name}</td>
+      <td>${empPayrollData._gender}</td>
+      <td>${getDeptHtml(empPayrollData._department)}</td>
+      <td>${empPayrollData._salary}</td>
+      <td>${empPayrollData._startDate}</td>
       <td>
-        <img id="1" onclick="remove(this)" alt="delete"
-          src="../assets/icon/delete-black-18dp.svg">
-        <img id="1"  alt="edit" onclick="update(this)"
-          src="../assets/icon/create-black-18dp.svg">
+          <img name="${empPayrollData._id}" onclick="remove(this)" alt="delete"
+              src="../assets/icon/delete-black-18dp.svg">
+          <img name="${empPayrollData._id}"  alt="edit" onclick="update(this)"
+              src="../assets/icon/create-black-18dp.svg">
       </td>
-    </tr>
+   </tr>
   `;
-    document.querySelector('#table-display').innerHTML = innerHtml;
+  }
+  document.querySelector('#table-display').innerHTML = innerHtml;
+}
+
+const creteEmployeePayrollJSON = () => {
+  let empPayrollListLocal = [
+      {
+          _name: 'Akhil',
+          _gender: 'Male',
+          _department: [
+              'Engineering',
+              'Finance'
+          ],
+          _salary: '500000',
+          _startDate: '29 Oct 2020',
+          _note: '',
+          _id: new Date().getTime(),
+          _profilePic: '../assets/profile-images/Ellipse -2.png'
+      },
+      {
+          _name: 'Keerthi Kumar',
+          _gender: 'female',
+          _department: [
+              'HR'
+          ],
+          _salary: '600000',
+          _startDate: '29 Oct 2019',
+          _note: '',
+          _id: new Date().getTime() + 1,
+          _profilePic: '../assets/profile-images/Ellipse -1.png'
+      }
+  ];
+  return empPayrollListLocal;
+}
+
+const getDeptHtml = (deptList) => {
+  let deptHtml = '';
+  for (const dept of deptList) {
+      deptHtml = `${deptHtml} <div class='dept-label'>${dept}</div>`
+  }
+  return deptHtml;
 }
