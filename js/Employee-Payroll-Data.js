043afbfafe477cba5e4231a6e2a542b4a5a2bc26
Dnyaneshwari <dnyaneshwarigaikwad5@gmail.com>
2021-03-04 21:16:57 +0530

    Added:Display Employee Details in tabular format

--- js/Employee-Payroll-Data.js
+++ js/Employee-Payroll-Data.js
@@ -1,76 +1,77 @@
 class EmployeePayrollData {
 
-  //getter and setter method
-  get id() { 
-   return this._id; 
-}
-set id(id) { 
-   this._id = id;
-}
-
-get name() {
-   return this._name;
-}
-set name(name) {
-   let nameRegex = RegExp('^[A-Z]{1}[a-zA-Z]{2,}$')
-   if(nameRegex.test(name))
-       this._name = name;
-   else throw ' Name is Incorrect ';
-}
-
-get profilePic() {
-   return this._profilePic;
-}
-set profilePic(profilePic) {
-   this._profilePic = profilePic;
-}
-
-get gender() {
-   return this._gender;
-}
-set gender(gender) {
-   this._gender = gender;
-}
-
-get department() {
-   return this._department;
-}
-set department(department) {
-   this._department = department;
-}
-
-get salary() {
-   return this._salary;
-}
-set salary(salary) {
-   this._salary = salary;
-}
-
-get notes() {
-   return this._notes;
-}
-set notes(note) {
-   this._notes = note;
-}
-
-get startDate() {
-   return this._startDate;
-}
-set startDate(startDate) {
-   let now = new Date();
-   console.log(now);
-   console.log(startDate);
-   if(startDate > now ) throw 'Start Date is a Future Date'
-   else
-   this._startDate = startDate;
-}
-
-//method
-toString() {
-   const options = { year: 'numeric', month: 'long', day: 'numeric' };
-   const empDate = !this.startDate ? "undefined" : this.startDate.toLocalDateString("en-US", options);
-   return "id = "+this.id +", name=" + this.name + ", gender=" + this.gender + 
-           ", profilePic=" + this.profilePic + ", department=" + this.department +
-           ", salary=" + this.salary + ", startDate=" + empDate + ", notes=" + this._notes;
- }
-}
+    //getter and setter method
+    get id() { 
+     return this._id; 
+  }
+  set id(id) { 
+     this._id = id;
+  }
+  
+  get name() {
+     return this._name;
+  }
+  set name(name) {
+     let nameRegex = RegExp('^[A-Z]{1}[a-zA-Z]{2,}$')
+     if(nameRegex.test(name))
+         this._name = name;
+     else throw ' Name is Incorrect ';
+  }
+  
+  get profilePic() {
+     return this._profilePic;
+  }
+  set profilePic(profilePic) {
+     this._profilePic = profilePic;
+  }
+  
+  get gender() {
+     return this._gender;
+  }
+  set gender(gender) {
+     this._gender = gender;
+  }
+  
+  get department() {
+     return this._department;
+  }
+  set department(department) {
+     this._department = department;
+  }
+  
+  get salary() {
+     return this._salary;
+  }
+  set salary(salary) {
+     this._salary = salary;
+  }
+  
+  get notes() {
+     return this._notes;
+  }
+  set notes(note) {
+     this._notes = note;
+  }
+  
+  get startDate() {
+     return this._startDate;
+  }
+  set startDate(startDate) {
+     let now = new Date();
+     console.log(now);
+     console.log(startDate);
+     if(startDate > now ) throw 'Start Date is a Future Date'
+     else
+     this._startDate = startDate;
+  }
+  
+  //method
+  toString() {
+     const options = { year: 'numeric', month: 'long', day: 'numeric' };
+     const empDate = !this.startDate ? "undefined" : this.startDate.toLocalDateString("en-US", options);
+     return "id = "+this.id +", name=" + this.name + ", gender=" + this.gender + 
+             ", profilePic=" + this.profilePic + ", department=" + this.department +
+             ", salary=" + this.salary + ", startDate=" + empDate + ", notes=" + this._notes;
+   }
+  }
+  
